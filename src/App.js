e12be83ff1f4ebb230ec77f8276900a2315d4ed8
Katherine Lee <katherinelee@Katherines-MacBook-Pro.local>
2024-10-17 01:06:39 -0500

    feat: add banner component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import { HorizontalNavBar } from './components/HorizontalNavBar';
+import { Banner } from './components/Banner';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <HorizontalNavBar />
+      <Banner />
     </div>
   );
 }
