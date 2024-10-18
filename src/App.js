faa3ccbf663665b63938468517a9f2a77c3959ff
Katherine Lee <katherinelee@Katherines-MacBook-Pro.local>
2024-10-18 01:01:46 -0500

    feat: add contact component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { HorizontalNavBar } from './components/HorizontalNavBar';
 import { Banner } from './components/Banner';
 import { Skills } from './components/Skills';
 import { Projects } from './components/Projects';
+import { Contact } from './components/Contact';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Banner />
       <Skills />
       <Projects />
+      <Contact />
     </div>
   );
 }
